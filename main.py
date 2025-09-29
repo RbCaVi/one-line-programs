e268d80c4fb5b5dda3e8850b79d5b719d5458eea
RbCaViTerminal <robert@robertvail.info>
2025-09-28 22:23:55 -0400

    /project create command works now (i guess)
    now we're in version 0.1.0 i guess

--- main.py
+++ main.py
@@ -1,50 +1,192 @@
 import discord
-from discord.ext import commands
+import discord.ext.commands
+import contextlib
+import json
+import os
+import random
+import string
 
 # https://stackoverflow.com/a/71169236
 
 guild_ids = [1203529312016400435] # just crest for now
+projects_info_path = 'projects.json'
+files_path = 'files/'
 
-def slash_command(name, *args, **kwargs):
-  return bot.slash_command(*args, name = name, **kwargs)
+@contextlib.contextmanager
+def command_group(bot, name, **kwargs):
+  group = bot.create_group(name = name, **kwargs)
+  yield CommandGroup(group)
 
-bot = commands.Bot()
+def slash_command(bot, name, **kwargs):
+  return bot.slash_command(name = name, guild_ids = guild_ids, **kwargs)
 
-@slash_command('first_slash')
+class CommandGroup:
+  def __init__(self, group):
+    self.group = group
+
+  @contextlib.contextmanager
+  def command_group(self, name, **kwargs):
+    group = self.group.create_subgroup(name = name, **kwargs)
+    yield CommandGroup(group)
+
+  def slash_command(self, name, **kwargs):
+    return self.group.command(name = name, guild_ids = guild_ids, **kwargs)
+
+class Projects:
+  def __init__(self):
+    self.projects = []
+    try:
+      with open(projects_info_path) as f:
+        projects_data = json.load(f)
+      self.projects = [Project.load(p) for p in projects_data]
+    except Exception as e:
+      print(f'Could not load projects from {projects_info_path}.')
+      print(f'Error: {e}')
+    self.projects_by_channel_id = {p.channel_id: p for p in self.projects}
+    self.projects_by_name = {p.name: p for p in self.projects}
+
+  def save(self):
+    with open(projects_info_path, 'w') as f:
+      json.dump([p.dump() for p in self.projects], f, indent = 2)
+
+  def __contains__(self, project):
+    if isinstance(project, int):
+      return project in self.projects_by_channel_id
+    if isinstance(project, str):
+      return project in self.projects_by_name
+    return (
+      project.channel_id in self.projects_by_channel_id or
+      project.name in self.projects_by_name
+    )
+
+  def add(self, project):
+    assert project not in self
+    self.projects.append(project)
+    self.projects_by_channel_id[project.channel_id] = project
+    self.projects_by_name[project.name] = project
+    project.files.append(File.new('main'))
+
+  def __getitem__(self, i):
+    # get a project by either name or channel id
+    if i in self.projects_by_channel_id:
+      return self.projects_by_channel_id[i]
+    elif i in self.projects_by_name:
+      return self.projects_by_name[i]
+    else:
+      raise ValueError(f'No project exists for key {repr(i)}')
+
+class Project:
+  def __init__(self, channel_id, name, files = None):
+    self.channel_id = channel_id
+    self.name = name
+    self.files = files or []
+
+  @staticmethod
+  def load(project_data):
+    return Project(
+      channel_id = project_data['channel_id'],
+      name = project_data['name'],
+      files = [File.load(f) for f in projects_data['files']],
+    )
+
+  def dump(self):
+    return {
+      'channel_id': self.channel_id,
+      'name': self.name,
+      'files': [f.dump() for f in self.files],
+    }
+
+class File:
+  def __init__(self, name, real_name):
+    self.name = name
+    self.real_name = real_name
+    with open(os.path.join(files_path, self.real_name)) as f:
+      lines_data = json.load(f)
+    self.lines = [Line.load(l) for l in lines_data]
+
+  @staticmethod
+  def load(file_data):
+    return File(
+      name = file_data['name'],
+      realname = file_data['realname'],
+    )
+
+  @staticmethod
+  def new(name):
+    # create an empty file
+    # how do i generate a realname
+    # idk just take name and put a "token" after it
+    while os.path.exists(os.path.join(files_path, real_name := name + '_' + ''.join(random.choice(string.ascii_lowercase) for i in range(8)))): # this is the best way to write this
+      pass
+    with open(os.path.join(files_path, real_name), 'w') as f:
+      json.dump([], f, indent = 2)
+    return File(
+      name = name,
+      real_name = real_name,
+    )
+
+  def dump(self):
+    return {
+      'name': self.name,
+      'real_name': self.real_name,
+    }
+
+  def save(self):
+    with open(os.path.join(files_path, self.real_name), 'w') as f:
+      json.dump([l.dump() for l in self.lines], f, indent = 2)
+
+bot = discord.ext.commands.Bot()
+
+projects = Projects()
+
+@slash_command(bot, 'first_slash')
 async def first_slash(ctx):
   await ctx.respond('You executed the slash command first_slash!')
 
-@slash_command('create_project')
-async def create_project(ctx):
-  await ctx.respond('You executed the slash command create_project!')
+with command_group(bot, 'project') as projectgroup:
+  @projectgroup.slash_command('new')
+  async def project_new(ctx, name: str):
+    print(f'Attempting to create a project named `{name}` in channel id {ctx.channel.id} ({ctx.channel.name})')
+    new_project = Project(ctx.channel.id, name)
+    if new_project not in projects:
+      projects.add(new_project)
+      projects.save()
+      await ctx.respond(f'Project `{new_project.name}` created.')
+    else:
+      if new_project.channel_id in projects:
+        await ctx.respond(f'A project has already been created in this channel: `{projects[new_project.channel_id].name}`')
+      elif new_project.name in projects:
+        await ctx.respond(f'A project has already been created with this name in <#{projects[new_project.name].channel_id}>')
 
-@slash_command('add_statement')
-async def add_statement(ctx):
-  await ctx.respond('You executed the slash command add_statement!')
+  @projectgroup.slash_command('files')
+  async def project_files(ctx):
+    await ctx.respond('You executed the slash command show_files!')
 
-@slash_command('edit_statement')
-async def edit_statement(ctx):
-  await ctx.respond('You executed the slash command edit_statement!')
+with command_group(bot, 'file') as filegroup:
+  @filegroup.slash_command('new')
+  async def file_new(ctx):
+    await ctx.respond('You executed the slash command add_file!')
 
-@slash_command('delete_statement')
-async def delete_statement(ctx):
-  await ctx.respond('You executed the slash command delete_statement!')
+  @filegroup.slash_command('delete')
+  async def file_delete(ctx):
+    await ctx.respond('You executed the slash command delete_file!')
 
-@slash_command('add_file')
-async def add_file(ctx):
-  await ctx.respond('You executed the slash command add_file!')
+  @filegroup.slash_command('view')
+  async def file_view(ctx):
+    await ctx.respond('You executed the slash command view_file!')
 
-@slash_command('delete_file')
-async def delete_file(ctx):
-  await ctx.respond('You executed the slash command delete_file!')
+with command_group(bot, 'statement') as statementgroup:
+  @statementgroup.slash_command('new')
+  async def statement_new(ctx):
+    await ctx.respond('You executed the slash command add_statement!')
 
-@slash_command('show_files')
-async def show_files(ctx):
-  await ctx.respond('You executed the slash command show_files!')
+  @statementgroup.slash_command('edit')
+  async def statement_edit(ctx):
+    await ctx.respond('You executed the slash command edit_statement!')
 
-@slash_command('view_file')
-async def view_file(ctx):
-  await ctx.respond('You executed the slash command view_file!')
+  @statementgroup.slash_command('delete')
+  async def statement_delete(ctx):
+    await ctx.respond('You executed the slash command delete_statement!')
 
 @bot.event
 async def on_ready() -> None:
